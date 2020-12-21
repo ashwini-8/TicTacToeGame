b45e89e67e422f81972f56c090372f859a5f6755
ashwini-8 <ashwinip309@gmail.com>
2020-12-21 08:39:44 +0530

    Added:UC1 create empty board of size 10

--- TicTicToeGame.java
+++ TicTicToeGame.java
@@ -1,7 +1,17 @@
 package com.project.regex;
 
 public class TicTicToeGame {
-    public  static void main(String[]args)  {
+    public  static void main(String[]args){
         System.out.println("Welcome to Tic Tac Toe Game");
+        char[] board= createBoard();
     }
-}
+    //uc1 - create empty 10 size array for board
+    private static char[] createBoard() {
+        char[] board = new char[10];
+        for (int i = 1; i < board.length; i++) {
+            board[i] = ' ';
+        }
+        System.out.println(board);
+        return board;
+            }
+    }
