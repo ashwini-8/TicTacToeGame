bf56661431830e1f0b53745632cea2c03227ceae
ashwini-8 <ashwinip309@gmail.com>
2020-12-21 08:50:31 +0530

    Added:UC2 Choose letter X or O

--- TicTicToeGame.java
+++ TicTicToeGame.java
@@ -1,9 +1,14 @@
 package com.project.regex;
 
+import java.util.Scanner;
+
 public class TicTicToeGame {
     public  static void main(String[]args){
         System.out.println("Welcome to Tic Tac Toe Game");
         char[] board= createBoard();
+        Scanner userInput = new Scanner(System.in);
+        char userLetter = chooseUserLetter(userInput);
+        char computerLetter = (userLetter == 'X') ? 'O' : 'X';
     }
     //uc1 - create empty 10 size array for board
     private static char[] createBoard() {
@@ -14,4 +19,9 @@ public class TicTicToeGame {
         System.out.println(board);
         return board;
             }
+    //uc2 choose letter X or O
+    private static char chooseUserLetter(Scanner userInput){
+        System.out.println("Choose your letter");
+        return userInput.next().toUpperCase().charAt(0);
+    }
     }
