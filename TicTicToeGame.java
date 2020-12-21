7d448fb115e8d576f14e9ddc27a48bec3f3abd01
ashwini-8 <ashwinip309@gmail.com>
2020-12-21 09:19:53 +0530

    Added:Ability for user to make desired move

--- TicTicToeGame.java
+++ TicTicToeGame.java
@@ -1,5 +1,6 @@
 package com.project.regex;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TicTicToeGame {
@@ -10,6 +11,7 @@ public class TicTicToeGame {
         char userLetter = chooseUserLetter(userInput);
         char computerLetter = (userLetter == 'X') ? 'O' : 'X';
         showBoard(board);
+        int userMove = getUserMove(board , userInput);
     }
     //uc1 - create empty 10 size array for board
     private static char[] createBoard() {
@@ -20,17 +22,30 @@ public class TicTicToeGame {
         System.out.println(board);
         return board;
             }
-    //uc2 choose letter X or O
+    //uc2 - choose letter X or O
     private static char chooseUserLetter(Scanner userInput){
         System.out.println("Choose your letter");
         return userInput.next().toUpperCase().charAt(0);
         }
-    //uc-3 print tic-tae-toe board
+    //uc3 -print tic-tae-toe board
     public static void showBoard(char[] board) {
         System.out.println(" |  " + board[1] + "  |  " + board[2] + "  |  " + board[3] + "  |");
         System.out.println(" |-----|-----|-----| ");
         System.out.println(" |  " + board[4] + "  |  " + board[5] + "  |  " + board[6] + "  |");
         System.out.println(" |-----|-----|-----| ");
         System.out.println(" |  " + board[7] + "  |  " + board[8] + "  |  " + board[9] + "  |");
+        }
+    //uc4 - get user move
+    private static int getUserMove(char [] board, Scanner userInput) {
+        Integer[] validCells = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        while (true) {
+            System.out.println("What is your next move? (1-9): ");
+            int index = userInput.nextInt();
+            if (Arrays.asList(validCells).contains(index) && isSpaceFree(board, index))
+                return index;
+            }
+        }
+    public static boolean isSpaceFree(char[] gameBoard, int position) {
+        return gameBoard[position] == ' ';
         }
     }
